85630bdf1d7fe227725d0fef2f36f4c1d031fc20
Des Green <>
2022-05-16 23:03:47 +0800

    component 16 Mei 2022 23:00 WITA

---
+++ message_comp.js
@@ -0,0 +1,20 @@
+Vue.component('message',{
+    props: ['title','body'],
+    template:
+        `
+        <article class="message">
+            <div class="message-header">
+                <p>{{title}}</p>
+                <button class="delete" aria-label="delete"></button>
+            </div>
+            <div class="message-body">
+                {{body}}
+            </div>
+        </article>
+        
+        `
+});
+
+new Vue({
+    el: '#root'
+});
